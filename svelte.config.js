8f30975cd546e57c2fffeb0bda0cb063fc1d689f
Mario Fučkan <mario.fucchan15@gmail.com>
2025-08-12 15:00:13 +0200

    Using Vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
-import adapter_vercel from "@sveltejs/adapter-vercel"
-import adapter_bun from "svelte-adapter-bun"
+import adapter from "@sveltejs/adapter-vercel"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
-import { env } from "bun"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +10,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: env.NODE_ENV == "development" ? adapter_bun() : adapter_vercel()
+		adapter: adapter()
 	},
 }
 
