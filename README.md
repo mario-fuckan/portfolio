4d118781a850bd8d562ea72baf8805071baaa682
Mario Fučkan <mario.fucchan15@gmail.com>
2025-08-12 16:52:25 +0200

    Added WhatsApp link

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Todo
-
-1. Create a WhatsApp Business Account and create a bot that responds to the first message sent and implement the WhatsApp button functionality.
--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -6,7 +6,9 @@
 <section id="contact">
 	<h1>Contact</h1>
 	<div class="contact__block">
-		<p>Best way to reach me is through: <a href={`mailto:${general.email}`}>{general.email}</a></p>
-		<a href="#"><IcBaselineWhatsapp /> WhatsApp</a>
+		<p>
+			Best way to reach me is through: <a href={`mailto:${general.email}`}>{general.email}</a>
+		</p>
+		<a href="https://wa.me/0976648032" target="_blank"><IcBaselineWhatsapp /> WhatsApp</a>
 	</div>
 </section>
