7015f15f1c0367a1c2ea959e2af7798b3248bfcd
Mario Fučkan <mario.fucchan15@gmail.com>
2025-08-12 14:54:27 +0200

    Bun for local, Vercel for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from "svelte-adapter-bun"
+import adapter_vercel from "@sveltejs/adapter-vercel"
+import adapter_bun from "svelte-adapter-bun"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
+import { env } from "bun"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,8 +12,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: env.NODE_ENV == "development" ? adapter_bun() : adapter_vercel()
+	},
 }
 
 export default config
