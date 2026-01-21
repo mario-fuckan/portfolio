d98ef70fe1fe9fa8a46434dd75f5df6e3fbaf88f
Mario Fučkan <mario.fucchan15@gmail.com>
2026-01-21 22:28:31 +0100

    Updated experience

--- src/lib/components/Introduction.svelte
+++ src/lib/components/Introduction.svelte
@@ -7,7 +7,7 @@
 	<h2>Full-Stack Developer</h2>
 	<hr />
 	<div class="introduction__stats">
-		{#each [{ title: "Croatia", stat: "Location" }, { title: "3+ months", stat: "Experience" }, { title: "5+", stat: "Projects" }] as { title, stat }}
+		{#each [{ title: "Croatia", stat: "Location" }, { title: "6+ months", stat: "Experience" }, { title: "5+", stat: "Projects" }] as { title, stat }}
 			<div class="stat">
 				<h3>{title}</h3>
 				<p>{stat}</p>
